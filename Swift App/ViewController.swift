8d131b65b58307218c96635ef0b9cede4f052300
My fIrst commit <thecunards@Andy-Cunards-iMac.local>
2017-05-02 11:08:47 -0400

    no message

---
+++ Swift App/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  Swift App
+//
+//  Created by Andy Cunard on 1/17/17.
+//  Copyright © 2017 SgtJAC777. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var theLabel: UILabel!
+    
+    var tapCount = 0
+    
+    @IBAction func buttonTapped(_ sender: UIButton) {
+
+     tapCount = tapCount + 1
+     
+        if tapCount >= 10 {
+            theLabel.text = "You tapped the button 10 times!!!"
+        }
+    }
+        
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+       
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
